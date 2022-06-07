341037edd78ff95a20301f578ba47e6b899e5445
Jabril Abdilahi <94321684+Jabrilthecoder@users.noreply.github.com>
2022-06-07 13:22:32 -0500

    Add files via upload

---
+++ Jabril.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const booking = [];
+
+
+const creatBooking = function(flightNum, numPassengers = 398, price = 2986) {
+
+
+    
+numPassengers = numPassengers || 1;
+
+    const booking = {
+    flightNum, 
+    numPassengers,
+    price
+}
+
+console.log(booking);
+booking.push(booking);
+
+
+}
+
+createBooking('DAL1881');
+createBooking('DAL1881', 523);
+createBooking('DAL1881', 2 );
+createBooking('DAL1881', 5 );
+  
+createBooking('DAL1881', underfined,  1000 );
+
+//129
+
+
+const flight = 'DAL1881'
+const Jabril = {
+
+name: 'Jabril Abdilahi',
+passport: 24522456798765
+}
+
+const checkIn = function(flightNum, passenger){
+flightNum = 'DAL 1787';
+passenger.name = 'Mr.' + passenger.name;
+
+if(passenger.passport === 24522456798765) {
+    alert
+}else ('WRONG PASSPORT!!!')
+};
+checkIng(flight, Jabril);
+console.log(flight);
+console.log(Jabril);
+
+
+//flightNum = flight;
+const flightNum = flight;
+const passenger = Jabril
+
+const newPassport = function(person) {
+    person.passport = Math.trunc(Math.random() = 100000000000);
+
+};
+
+newPassport(Jabril);
+checkIng(flight, Jabril);
